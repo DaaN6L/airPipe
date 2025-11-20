b3f3d0fc644c4b73f6c1c6184a364b2e8f38afc6
Kar298 <sazametki@gmail.com>
2025-11-20 14:00:51 +0200

    Add files via upload

---
+++ proj1.ino
@@ -0,0 +1,145 @@
+#include <Adafruit_HX711.h>
+#include <GyverPortal.h>
+#include <WiFi.h>  
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+
+#define AP_SSID "LEV"
+#define AP_PASS "L72V73E08"
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+#define DATA_PIN 2
+#define CLOCK_PIN 3
+
+GyverPortal ui;
+Adafruit_BME280 bme;
+Adafruit_HX711 hx711(DATA_PIN, CLOCK_PIN);
+
+uint8_t valSlider;
+uint8_t button1;
+uint8_t button2;
+uint16_t valSpinner;
+uint16_t weight;
+uint16_t weight_sr;
+float speed;
+uint16_t pressure;
+float pres_sr;        
+unsigned long startTime;
+bool ramping = 0;
+bool ram = 0;
+bool tare = 0;
+const char *pl1 [] = {"Швидкість потоку"};
+const char *pl2 [] = {"Маса"};
+
+void build() {
+  GP.BUILD_BEGIN(1000);
+  GP.UPDATE("weight, speed");
+  GP.TITLE("Аеротруба", "t1", "#7CFC00", 35);
+  GP.HR();
+  GP.THEME(GP_DARK);
+  GP.BUTTON("btn1", "Калібрувати"); GP.BREAK();
+  GP.LABEL("Потужність: ");
+  GP.SLIDER("sld1", valSlider, 0, 100); GP.BREAK();
+  GP.LABEL("Час: ");
+  GP.SPINNER("spn1", valSpinner, 0, 300, 5); 
+  GP.LABEL("c"); GP.BREAK();
+  GP.BUTTON("btn2", "Почати дослід"); GP.BREAK();
+  GP.LABEL("Маса: ");
+  GP.LABEL("weight", "weight", "#7CFC00", 28);     
+  GP.LABEL("г"); GP.BREAK();
+  GP.LABEL("Швидкість: ");
+  GP.LABEL("speed", "speed", "#7CFC00", 28);     
+  GP.LABEL("м/с"); GP.BREAK();
+  GP.AJAX_PLOT_DARK("plot1", pl1, 1, 25, 1000);
+  GP.AJAX_PLOT_DARK("plot2", pl2, 1, 25, 1000);
+  GP.BUILD_END();
+}
+
+void setup() {
+  Serial.begin(115200);
+  hx711.begin();
+  bme.begin();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(AP_SSID, AP_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+  for (uint8_t i = 0; i < 3; i++) {
+    hx711.tareA(hx711.readChannelRaw(CHAN_A_GAIN_128));
+    hx711.tareA(hx711.readChannelRaw(CHAN_A_GAIN_128));   // тарування
+  }
+  ui.attachBuild(build);
+  ui.attach(action);
+  ui.start();
+  startTime = millis();
+}
+
+void action() {
+   if (ui.update()) {
+    ui.updateInt("weight", weight_sr);
+    ui.updateInt("speed", speed);
+  }
+  if (ui.update("plot1")) ui.answer(random(200)); // рандом для тесту
+  if (ui.update("plot2")) ui.answer(random(150)); // рандом для тесту
+  if (ui.clickInt("sld1", valSlider)) {
+      Serial.print("Slider: ");
+      Serial.println(valSlider);
+  }
+  if (ui.clickInt("spn1", valSpinner)) {
+      Serial.print("Spinner: ");
+      Serial.println(valSpinner);
+  }
+  if (ui.click("btn1")) {
+    Serial.println("Button 1 click");
+    tare = 1;
+  }
+  if (ui.click("btn2")) {
+    Serial.println("Button 2 click");
+    ramping = 1;
+    ram = 1;
+  }
+}
+
+void loop() {
+  ui.tick();
+
+  if (ramping) {
+    if (ram) {
+      startTime = millis();
+      ram = 0;
+    }
+    unsigned long elapsed = millis() - startTime;
+    if (elapsed >= valSpinner * 1000UL) {
+      ramping = 0;
+      return;
+    }
+    float progress = float(elapsed) / (valSpinner * 1000.0); // тут ми плавно збільшуємо потужність
+    float currentPower = progress * valSlider;
+    Serial.println(currentPower);
+  }
+
+  if (tare) {
+    for (uint8_t i = 0; i < 3; i++) {
+      hx711.tareA(hx711.readChannelRaw(CHAN_A_GAIN_128));   // тарування
+    }
+    tare = 0;
+  }
+
+  weight = 0;
+  for(uint8_t i = 0; i < 5; i++) {
+    weight += hx711.readChannelBlocking(CHAN_A_GAIN_128); // усереднення по 5 значенням
+  }
+  weight_sr = pressure/10;
+
+  pressure = 0;
+  for(uint8_t i = 0; i < 10; i++) {
+    pressure += bme.readPressure(); // усереднення по 10 значенням
+  }
+  pres_sr = 1,66*(pressure/10);
+  speed=sqrt(pres_sr); // швидкість потоку повітря за формуллою для трубки Піто. 1,66 - приблизне значення 2/густину повітря при норм. умовах
+
+}
